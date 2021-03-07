a8ad5a297b5210f52b0eeeef051764b6b8eaf4d7
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2021-03-07 11:03:47 -0500

    more fetures

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,18 +1,24 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +26,7 @@ import java.util.List;
 import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
+    private final int REQUEST_CODE = 20;
     public static final String TAG = "TimelineActivity";
     TwitterClient client;
     RecyclerView rvTweets;
@@ -99,6 +106,40 @@ public class TimelineActivity extends AppCompatActivity {
             }
         }, tweets.get(tweets.size()-1).id);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; add items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== R.id.compose) {
+            // Compose icon has been selected
+            // Navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            //Get data from the intent(tweet)
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            // Update the RV with the tweet
+            // Modify data source of tweets
+            tweets.add(0,tweet);
+            // Update the adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void populateHomeTimeLine() {
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,16 +1,36 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
+@Entity
 public class User {
 
+    @ColumnInfo
+    @PrimaryKey
+    public static long id;
+
+    @ColumnInfo
     public String name;
+
+    @ColumnInfo
     public String screenName;
+
+    @ColumnInfo
     public String profileImageUrl;
+
+    //empty constructor needed by the Parceler Library
+    public User() {}
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
+        User.id = jsonObject.getLong("id");
         user.name = jsonObject.getString("name");
         user.screenName = "@" + jsonObject.getString("screen_name");
         user.profileImageUrl = jsonObject.getString("profile_image_url_https");
